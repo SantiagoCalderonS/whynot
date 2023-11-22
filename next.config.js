7228e3051b1240091570600072495cdaa00d0af3
SantiagoCalderonS <caldesanche@gmail.com>
2023-11-21 22:06:53 -0500

    headers

--- next.config.js
+++ next.config.js
@@ -5,18 +5,24 @@ const nextConfig = {
             {
                 // coincidiendo con todas las rutas de API
                 source: "/api/:path*",
-                headers: [
-                  // omitido por brevedad...
-                ]
-            },
-            {
-                source: "/api/special-data",
-                headers: [
-                   { key: "Access-Control-Allow-Credentials", value: "false" },
-                   { key: "Access-Control-Allow-Origin", value: "https://example.com" },
-                   { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                   { key: "Access-Control-Allow-Headers", value: "Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date" },
-                ]
+              headers: [
+                 {
+                     key: "Access-Control-Allow-Credentials",
+                     value: "true"
+                 },
+                 {
+                     key: "Access-Control-Allow-Origin",
+                     value: "*" // reemplaza esto por tu origen real
+                 },
+                 {
+                     key: "Access-Control-Allow-Methods",
+                     value: "GET,DELETE,PATCH,POST,PUT"
+                 },
+                 {
+                     key: "Access-Control-Allow-Headers",
+                     value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+                 }
+              ]
             }
         ]
     }
